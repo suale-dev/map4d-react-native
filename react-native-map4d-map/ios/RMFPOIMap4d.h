7df51192ec557d19ba72fc5816e05a09b03652a8
omyto <omyto@pm.me>
2020-07-05 22:29:25 +0700

    - ios: add poi

---
+++ react-native-map4d-map/ios/RMFPOIMap4d.h
@@ -0,0 +1,22 @@
+//
+//  RMFPOIMap4d.h
+//  Map4dMap
+//
+//  Created by Huy Dang on 7/5/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#ifndef RMFPOIMap4d_h
+#define RMFPOIMap4d_h
+
+#import <Map4dMap/Map4dMap.h>
+
+@class RMFPOI;
+
+@interface RMFPOIMap4d : MFPOI
+
+@property (nonatomic, weak) RMFPOI * reactPOI;
+
+@end
+
+#endif /* RMFPOIMap4d_h */
---
+++ react-native-map4d-map/ios/RMFPOIMap4d.m
@@ -0,0 +1,12 @@
+//
+//  RMFPOIMap4d.m
+//  Map4dMap
+//
+//  Created by Huy Dang on 7/5/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import "RMFPOIMap4d.h"
+
+@implementation RMFPOIMap4d
+@end
