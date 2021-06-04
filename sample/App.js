7fa8b0fa2c1417bf4b3d3cd378219c36679b8b8f
Duy Dung <dungvd@iotlink.com.vn>
2021-06-04 17:11:13 +0700

    - update sample for android to build success

--- sample/App.js
+++ sample/App.js
@@ -1,89 +1,28 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-import 'react-native-gesture-handler';
 import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
-  Image,
-  View,
-  Text,
-  Button,
-  NativeModules,
+  StyleSheet
 } from 'react-native';
 
+import {MFMapView} from 'react-native-map4d-map';
 
-import MapScreen from './MapScreen'
-
-
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-const { Navigation } = require('react-native-navigation');
-
-
-function HomeScreen() {
+function App() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
+    <>
+      <SafeAreaView style={styles.safeView}>
+        <MFMapView style={styles.container}/>
+      </SafeAreaView>
+    </>
   );
-}
-Navigation.registerComponent('Home', () => HomeScreen);
-Navigation.registerComponent('Map', () => MapScreen);
+};
 
-// const Stack = createStackNavigator();
+const styles = StyleSheet.create({
+  safeView: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+  },
+});
 
-//From NPM
-/*
-import {MFMapView, MFMarker, MFCircle, MFPolyline} from 'react-native-map4d-map'
-*/
-
-// function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator initialRouteName="Map">
-//         <Stack.Screen name="Home" component={HomeScreen} />
-//         <Stack.Screen name="Map" component={MapScreen} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-// export default App;
-
-export default class App {
-  constructor() {
-    const stack = {
-      id:'AppStack',
-      children:[
-          {
-              component: {
-                  name: 'Map'
-              },
-              passProps: null,
-          }
-      ],
-      options: {
-          topBar: {
-              visible: false,
-              drawBehind: true,
-              animate: true,
-          }
-      }
-  }
-  // NavigationManager.addComponentId(stack.children[0].component.name)
-  Navigation.events().registerAppLaunchedListener(async () => {
-    Navigation.setRoot({
-      root: {
-        stack
-      }
-    });
-  });
-}
-}
-
+export default App;
